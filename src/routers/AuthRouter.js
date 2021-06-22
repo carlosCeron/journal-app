ae75ae451597d7788789a0024b1cd1a0cd9d09b2
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-06-21 23:59:56 -0500

    Ajustes scss

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Switch, Route, Redirect } from 'react-router-dom';
-import LoginScreen from './../components/auth/LoginScreen';
-import RegisterScreen from './../components/auth/RegisterScreen';
+import React from "react";
+import PropTypes from "prop-types";
+import { Switch, Route, Redirect } from "react-router-dom";
+import LoginScreen from "./../components/auth/LoginScreen";
+import RegisterScreen from "./../components/auth/RegisterScreen";
 
-const AuthRouter = props => {
+const AuthRouter = (props) => {
   return (
-    <Switch>
-      <Route exact path="/auth/login" component={LoginScreen} />
-      <Route exact path="/auth/register" component={RegisterScreen} />
-      <Redirect to="/auth/login" />
-    </Switch>
-  )
-}
+    <div className="auth__main">
+      <div className="auth__box-container">
+        <Switch>
+          <Route exact path="/auth/login" component={LoginScreen} />
+          <Route exact path="/auth/register" component={RegisterScreen} />
+          <Redirect to="/auth/login" />
+        </Switch>
+      </div>
+    </div>
+  );
+};
 
-AuthRouter.propTypes = {
+AuthRouter.propTypes = {};
 
-}
-
-export default AuthRouter
+export default AuthRouter;
